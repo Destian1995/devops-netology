91f32397c432e293876816cb03443b76d3296ab5
Destian <Lerdonia@mail.ru>
2022-10-21 13:21:16 +0400

    Commit

--- terraform-basic/main.tf
+++ terraform-basic/main.tf
@@ -2,6 +2,8 @@ provider "yandex" {
   cloud_id    = "b1gfodjsfjm1ue7u0ld7"
   folder_id   = "b1ggthdvv3nparichh2u"
   zone        = "ru-central1-a"
+  storage_access_key = ""
+  storage_secret_key = ""
 }
 
 resource "yandex_compute_instance" "instance-1" {
