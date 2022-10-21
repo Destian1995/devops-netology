757321cc5a5efe4025539fdd5d667fb96f3feb30
Destian <Lerdonia@mail.ru>
2022-10-21 12:58:02 +0400

    Переделал.....

--- terraform-basic/versions.tf
+++ terraform-basic/versions.tf
@@ -5,12 +5,4 @@ terraform {
       version = "~> 0.70.0"
     }
   }
-    backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = "Devops"
-    region     = "ru-central1"
-    key        = "devops-netology/terraform-basic/terraform.tfstate"
-    skip_region_validation      = true
-    skip_credentials_validation = true
-  }
 }
