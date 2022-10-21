6f77ad8158982eddf896f85b971640379b25d8e5
Destian <Lerdonia@mail.ru>
2022-10-21 14:06:19 +0400

    new arch

--- terraform-basic/network.tf
+++ terraform-basic/network.tf
@@ -1,10 +1,10 @@
-resource "yandex_vpc_network" "vpcnet" {
-  name = "vpcnet"
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
 }
 
-resource "yandex_vpc_subnet" "vpcsubnet" {
-  name           = "vpcsubnet"
-  network_id     = resource.yandex_vpc_network.vpcnet.id
-  v4_cidr_blocks = ["10.2.0.0/24"]
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
   zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
 }
